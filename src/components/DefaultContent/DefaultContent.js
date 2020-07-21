90a4a2edd36238200dcb03676277bb08421c4900
AlexZ2019 <zenchenkoalex96@gmail.com>
2020-07-21 21:39:57 +0300

    Justin portfolio (defaultContent and footer layout)

--- src/components/DefaultContent/DefaultContent.js
+++ src/components/DefaultContent/DefaultContent.js
@@ -42,21 +42,19 @@ class DefaultContent extends React.Component {
                     <a className="carousel-control-prev" href="#carouselExampleControls" role="button"
                        data-slide="prev">
                         <span className="carousel-control-prev-icon" aria-hidden="true">
-
                         </span>
                         <span className="sr-only">Previous</span>
                     </a>
                     <a className="carousel-control-next" href="#carouselExampleControls" role="button"
                        data-slide="next">
                         <span className="carousel-control-next-icon" aria-hidden="true">
-
                         </span>
                         <span className="sr-only">Next</span>
                     </a>
                 </div>
                 <div className={s["content"] + " container-fluid"}>
                     <div className={`row`}>
-                        <div className={s["content-block"] + " col-sm-4 col-12 offset-sm-1"}>
+                        <div className={s["content-block"] + " col-sm-4 col-10 offset-1"}>
                             <img src={departments} alt={departments}/>
                             <hr/>
                             <p>Наші відділення</p>
@@ -64,7 +62,7 @@ class DefaultContent extends React.Component {
                                 Пошук
                             </button>
                         </div>
-                        <div className={s["content-block"] + " col-sm-4 col-12 offset-sm-2"}>
+                        <div className={s["content-block"] + " col-sm-4 col-10 offset-1 offset-sm-2 mt-5 mt-sm-0"}>
                             <img src={calculator} alt="calculator"/>
                             <hr/>
                             <p>Калькулятор</p>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,35 +1,49 @@
 import React from "react";
 import s from "./Footer.module.css"
+import fbLogo from "../../images/facebook-logo.png"
+import instLogo from "../../images/instagram-logo.png"
+import telegramLogo from "../../images/telegram-logo.png"
+import messengerLogo from "../../images/messenger-logo.png"
+
 
 const Footer = () => {
     return <div className="container-fluid">
         <div className={s["footer"] + " row"}>
             <ul className="offset-md-1 offset-sm-1 offset-1 col-md-3 col-sm-5 col-11">
-                <li><a href="#"> About Justin</a></li>
-                <li><a href="#">Departments map</a></li>
-                <li><a href="#">Departments List</a></li>
-                <li><a href="#">Cost calculation</a></li>
+                <li><a href="#">Про Justin</a></li>
+                <li><a href="#">Карта відділень</a></li>
+                <li><a href="#">Список відділень</a></li>
+                <li><a href="#">Розрахунок ціни</a></li>
             </ul>
             <ul className="offset-sm-1 offset-1 col-md-3 col-sm-5 col-11">
-                <li><a href="#"> Tariffs</a></li>
-                <li><a href="#">Departments map</a></li>
-                <li><a href="#">Terms of service</a></li>
-                <li><a href="#">Make a contract</a></li>
+                <li><a href="#">Тарифи</a></li>
+                <li><a href="#">Список відділень</a></li>
+                <li><a href="#">Умови надання послуг</a></li>
+                <li><a href="#">Укласти договір</a></li>
             </ul>
-            <ul className="offset-sm-1 offset-1 col-md-3 col-sm-11 col-11">
-                <li><a href="#"> Our partners</a></li>
-                <li><a href="#">Credit intermediaries</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Contacts</a></li>
+            <ul className={s["list-without-borders"] + " offset-sm-1 offset-1 col-md-3 col-sm-11 col-11"}>
+                <li><a href="#">Наші партнери</a></li>
+                <li><a href="#">Кредитні посередники</a></li>
+                <li><a href="#">Новини</a></li>
+                <li><a href="#">Контакти</a></li>
             </ul>
+            <p className="offset-1 col-3">Justin © 2020</p>
             <p className="offset-1 col-3">
-                © 2020 Company Justin
+                <a href="#">
+                    <img src={fbLogo} alt="Facebook Logo"/>
+                </a>
+                <a href="#">
+                    <img src={instLogo} alt="Instagram Logo"/>
+                </a>
+                <a href="#">
+                    <img src={telegramLogo} alt="Telegram Logo"/>
+                </a>
+                <a href="#">
+                    <img src={messengerLogo} alt="Messenger Logo"/>
+                </a>
             </p>
             <p className="offset-1 col-3">
-                social networks
-            </p>
-            <p className="offset-1 col-3">
-                <a href="#">Privacy policy</a>
+                <a href="#">Політика конфіденційності</a>
             </p>
         </div>
     </div>
